a5ee98ffa138a396504746f1ea00fd08bf1b3905
Aidan Fell <islandmonkeee@gmail.com>
2024-12-15 17:18:35 +0000

    layout component + add to existing about and home blades

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,3 @@
-<!DOCTYPE HTML>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>About page</title>
-    </head>
-    <body>
-        <nav>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-        </nav>
-        <h1>Hello from the Home Page.</h1>
-    </body>
-</html>
+<x-layout>
+    <h1>Hello from the Home Page</h1>
+</x-layout>
