d6854d501811d0184067895e0cf78b0d07826b61
Aidan Fell <islandmonkeee@gmail.com>
2024-12-26 18:28:43 +0000

    blade changes

--- resources/views/about.blade.php
+++
@@ -1,3 +0,0 @@
-<x-layout>
-    <h1>Welcome to the About Page</h1>
-</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE HTML>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>About page</title>
-    </head>
-    <body>
-        <h1>Hello from the Contact Page.</h1>
-    </body>
-</html>
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    <h1>Welcome to the Contact Page</h1>
+</x-layout>
