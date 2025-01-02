707100a0d397f50e9c4fe4be483c0cd26b7d79a1
Aidan Fell <islandmonkeee@gmail.com>
2025-01-02 21:39:04 +0000

    shuffle contact view to top and remove commented out lines

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,5 @@ use App\Http\Controllers\JobListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-
-/*Route::controller(JobListingController::class)->group(function() {
-    Route::get('/jobs',  'index');
-    Route::get('/jobs/create',  'create');
-    Route::get('/jobs/{job}',  'show');
-    Route::post('/jobs',  'store');
-    Route::get('/jobs/{job}/edit',  'edit');
-    Route::patch('/jobs/{job}',  'update');
-    Route::delete('/jobs/{id}',  'destroy');
-});*/
-
-Route::resource('jobs', JobListingController::class);
-
-// Contact page
 Route::view('/contact', 'contact');
+Route::resource('jobs', JobListingController::class);
